788cb89bda8bea9a715792bf83c089ed306b7a1f
Susan Karin <susankarin.info@gmail.com>
2024-01-30 17:21:28 -0300

    matrizes

---
+++ atividadematriz/matriz.js
@@ -0,0 +1,9 @@
+let matrizalunos = [
+    ["aluno1", 5, 6, 7],
+    ["aluno2", 3, 4.5, 8.9],
+    ["aluno3", 9, 10, 8],
+    ["aluno4", 3, 2, 1.5]
+];
+
+const elemento = matrizalunos[1][1];
+console.log("elemento na posição (1, 1):"+ elemento);
